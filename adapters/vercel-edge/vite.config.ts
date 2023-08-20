108f6ccb0a8f4c34ea01ea982cf614d5fd161782
nkuek <nkuek1@gmail.com>
2023-08-20 00:29:06 -0400

    test fix

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -2,14 +2,20 @@ import { vercelEdgeAdapter } from "@builder.io/qwik-city/adapters/vercel-edge/vi
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
 import { builtinModules } from "module";
+import path from "path";
 
 export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
+      lib: {
+        entry: path.resolve(__dirname, 'index.ts'),
+        formats: ['es'],
+        fileName: () => 'out.js',
+      },
       rollupOptions: {
         input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
-        external: [...builtinModules, /^node:/]
+        external: [...builtinModules, ...builtinModules.map((m) => `node:${m}`)]
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
