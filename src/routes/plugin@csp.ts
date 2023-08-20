7baae482107ee3fd23a0cea3df7e94705f86dcb7
nkuek <nkuek1@gmail.com>
2023-08-20 00:39:43 -0400

    revert

--- src/routes/plugin@csp.ts
+++ src/routes/plugin@csp.ts
@@ -1,10 +1,9 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
 import { isDev } from '@builder.io/qwik/build';
-import crypto from 'node:crypto';
 
 export const onRequest: RequestHandler = (event) => {
   if (isDev) return; // Will not return CSP headers in dev mode
-  const nonce = crypto.randomBytes(16).toString('hex'); // Your custom nonce logic here
+  const nonce = Date.now().toString(36); // Your custom nonce logic here
   event.sharedMap.set('@nonce', nonce);
   const csp = [
     `font-src 'self' https://fonts.gstatic.com`,
