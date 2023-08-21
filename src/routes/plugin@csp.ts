5381763dbb6a700104ad01d8277a8c5a0abf7c74
nkuek <nkuek1@gmail.com>
2023-08-21 16:57:32 -0400

    improve nonce

--- src/routes/plugin@csp.ts
+++ src/routes/plugin@csp.ts
@@ -3,15 +3,16 @@
 
 import type { RequestHandler } from '@builder.io/qwik-city';
 import { isDev } from '@builder.io/qwik/build';
+import crypto from 'crypto';
 
 export const onRequest: RequestHandler = (event) => {
   if (isDev) return; // Will not return CSP headers in dev mode
-  const nonce = Date.now().toString(36); // Your custom nonce logic here
+  const nonce = crypto.randomBytes(16).toString('hex'); // Your custom nonce logic here
   event.sharedMap.set('@nonce', nonce);
   const csp = [
     `font-src 'self' https://fonts.gstatic.com`,
     `img-src 'self' https://res.cloudinary.com/dunbkcyqq/ data:`,
-    `script-src 'self' 'unsafe-inline' 'nonce-${nonce}'`,
+    `script-src 'self' http: https: 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic'`,
     `style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net 'unsafe-inline'`,
     `connect-src 'self' https://vitals.vercel-insights.com https://vitals.vercel-analytics.com/v1/vitals`,
     `script-src-attr 'none'`,
