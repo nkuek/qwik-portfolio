152b7db6af92eefd84040e3b5ef63e0260f5e65f
nkuek <nkuek1@gmail.com>
2023-08-28 22:44:18 -0400

    better article head

--- src/routes/snippets/(articles)/layout.tsx
+++ src/routes/snippets/(articles)/layout.tsx
@@ -1,9 +1,11 @@
 import { Slot, component$ } from '@builder.io/qwik';
 import { css } from '@styles/css';
+import ArticleHead from '~/components/articleHead';
 
 export default component$(() => {
   return (
     <article class={css({ minHeight: '100vh' })}>
+      <ArticleHead />
       <Slot />
     </article>
   );
