1ba336484db1433f4c7749005dd25c435243f818
nkuek <nkuek1@gmail.com>
2023-09-02 16:58:55 -0400

    move button helper to ds button

--- src/components/design-system/DSButton.tsx
+++ src/components/design-system/DSButton.tsx
@@ -10,10 +10,25 @@ export type DSButtonPropsBase = {
 type DSButtonProps = Omit<QwikIntrinsicElements['button'], 'class'> &
   DSButtonPropsBase;
 
+/**  PandaCSS requires classes to be set at runtime, so we need to explicitly set the button type we want instead of just passing in the variant directly */
+export function getButtonClassFromVariant(
+  variant: DSButtonPropsBase['variant']
+) {
+  switch (variant) {
+    case 'primary':
+      return button({ type: 'primary' });
+    case 'secondary':
+      return button({ type: 'secondary' });
+    case 'tertiary':
+      return button({ type: 'tertiary' });
+  }
+}
+
 export const DSButton = component$(
   ({ variant, class: classExtension, ...props }: DSButtonProps) => {
+    const buttonClass = getButtonClassFromVariant(variant);
     return (
-      <button class={cx(button({ type: variant }), classExtension)} {...props}>
+      <button class={cx(buttonClass, classExtension)} {...props}>
         <Slot />
       </button>
     );
--- src/components/design-system/DSLink.tsx
+++ src/components/design-system/DSLink.tsx
@@ -1,8 +1,10 @@
 import { type QwikIntrinsicElements, Slot, component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
 import { css, cx } from '@styles/css';
-import { button } from '@styles/recipes';
-import { type DSButtonPropsBase } from '~/components/design-system/DSButton';
+import {
+  getButtonClassFromVariant,
+  type DSButtonPropsBase,
+} from '~/components/design-system/DSButton';
 import { DSText, type DSTextProps } from '~/components/design-system/DSText';
 
 type DSLinkProps = Omit<QwikIntrinsicElements['a'], 'class'> &
@@ -33,18 +35,6 @@ export const DSLink = component$(
 );
 
 type DSButtonLinkProps = QwikIntrinsicElements['a'] & DSButtonPropsBase;
-
-/**  PandaCSS requires classes to be set at runtime, so we need to explicitly set the button type we want instead of just passing in the variant directly */
-function getButtonClassFromVariant(variant: DSButtonPropsBase['variant']) {
-  switch (variant) {
-    case 'primary':
-      return button({ type: 'primary' });
-    case 'secondary':
-      return button({ type: 'secondary' });
-    case 'tertiary':
-      return button({ type: 'tertiary' });
-  }
-}
 
 export const DSButtonLink = component$(
   ({ variant, class: classExtension, ...props }: DSButtonLinkProps) => {
